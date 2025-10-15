f7e9577498f03ae976144e4412448fc1b987ec58
feelsbadkot <bond1010a@gmail.com>
2025-10-15 19:02:28 +0500

    почти написал вычисление N и delta

--- src/returnTimeStatistics.jl
+++ src/returnTimeStatistics.jl
@@ -1,3 +1,20 @@
 module ReturnTimeStatistics
 
+using Peaks, Roots, BaseStats, Interpolations
+
+function filling_factor(signal, time)
+    indices, heights = findmaxima(signal)
+    diff(time[indices])
+    hist = fit(Histogram, data, nbins=bins)
+    edges = hist.edges[1]
+    bin_width = step(edges)
+    Nt = count(!iszero, hist.weights)
+    return Nt * bin_width
+end 
+
+function frequency_of_reversals(signal, time)
+    f = linear_interpolation(time, X, extrapolation_bc=Line())
+    find_zeros(f, time[0], time[end])
+end
+
 end 
